dff20676b7d8c66cd15d8eba5e1236965a746d74
Johannes Weiner <hannes@cmpxchg.org>
2016-01-20 15:02:18 -0800

    mm: memcontrol: drop unused @css argument in memcg_init_kmem
    This series adds accounting of the historical "kmem" memory consumers to
    the cgroup2 memory controller.
    
    These consumers include the dentry cache, the inode cache, kernel stack
    pages, and a few others that are pointed out in patch 7/8.  The
    footprint of these consumers is directly tied to userspace activity in
    common workloads, and so they have to be part of the minimally viable
    configuration in order to present a complete feature to our users.
    
    The cgroup2 interface of the memory controller is far from complete, but
    this series, along with the socket memory accounting series, provides
    the final semantic changes for the existing memory knobs in the cgroup2
    interface, which is scheduled for initial release in the next merge
    window.
    
    This patch (of 8):
    
    Remove unused css argument frmo memcg_init_kmem()
    
    Signed-off-by: Johannes Weiner <hannes@cmpxchg.org>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Cc: Tejun Heo <tj@kernel.org>
    Acked-by: Vladimir Davydov <vdavydov@virtuozzo.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -3586,7 +3586,7 @@ static int mem_cgroup_oom_control_write(struct cgroup_subsys_state *css,
 }
 
 #ifdef CONFIG_MEMCG_KMEM
-static int memcg_init_kmem(struct mem_cgroup *memcg, struct cgroup_subsys *ss)
+static int memcg_init_kmem(struct mem_cgroup *memcg)
 {
 	int ret;
 
@@ -3594,7 +3594,7 @@ static int memcg_init_kmem(struct mem_cgroup *memcg, struct cgroup_subsys *ss)
 	if (ret)
 		return ret;
 
-	return tcp_init_cgroup(memcg, ss);
+	return tcp_init_cgroup(memcg);
 }
 
 static void memcg_deactivate_kmem(struct mem_cgroup *memcg)
@@ -3656,7 +3656,7 @@ static void memcg_destroy_kmem(struct mem_cgroup *memcg)
 	tcp_destroy_cgroup(memcg);
 }
 #else
-static int memcg_init_kmem(struct mem_cgroup *memcg, struct cgroup_subsys *ss)
+static int memcg_init_kmem(struct mem_cgroup *memcg)
 {
 	return 0;
 }
@@ -4341,7 +4341,7 @@ mem_cgroup_css_online(struct cgroup_subsys_state *css)
 	}
 	mutex_unlock(&memcg_create_mutex);
 
-	ret = memcg_init_kmem(memcg, &memory_cgrp_subsys);
+	ret = memcg_init_kmem(memcg);
 	if (ret)
 		return ret;
 
