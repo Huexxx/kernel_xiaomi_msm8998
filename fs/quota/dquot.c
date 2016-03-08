656d3fb80ad2ce5aae2d82e53b3cdfc812161091
Al Viro <viro@zeniv.linux.org.uk>
2016-03-07 23:27:22 -0500

    quota: use lookup_one_len_unlocked()
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/quota/dquot.c
+++ fs/quota/dquot.c
@@ -2428,9 +2428,7 @@ int dquot_quota_on_mount(struct super_block *sb, char *qf_name,
 	struct dentry *dentry;
 	int error;
 
-	mutex_lock(&d_inode(sb->s_root)->i_mutex);
-	dentry = lookup_one_len(qf_name, sb->s_root, strlen(qf_name));
-	mutex_unlock(&d_inode(sb->s_root)->i_mutex);
+	dentry = lookup_one_len_unlocked(qf_name, sb->s_root, strlen(qf_name));
 	if (IS_ERR(dentry))
 		return PTR_ERR(dentry);
 
