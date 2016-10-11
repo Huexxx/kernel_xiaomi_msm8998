f04c6c98ba0f6d63c2c9a27d498f05f34559ff1e
Amit Pundir <amit.pundir@linaro.org>
2016-10-11 13:26:17 +0530

    ANDROID: sdcardfs: Propagate dentry down to inode_change_ok()
    Since commit 31051c85b5e2 ("fs: Give dentry to inode_change_ok()
    instead of inode"), to avoid clearing of capabilities or security
    related extended attributes too early, inode_change_ok() will
    need to take dentry instead of inode. Propagate it down to
    sdcardfs_setattr() and also rename it to setattr_prepare(),
    otherwise we run into following build error:
    
      CC [M]  fs/sdcardfs/inode.o
    fs/sdcardfs/inode.c: In function ‘sdcardfs_setattr’:
    fs/sdcardfs/inode.c:644:8: error: implicit declaration of function ‘inode_change_ok’ [-Werror=implicit-function-declaration]
      err = inode_change_ok(inode, ia);
            ^
    
    Change-Id: I714b4f4f68b7fea1ac82a71d2f323c76b11fa008
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

--- fs/sdcardfs/inode.c
+++ fs/sdcardfs/inode.c
@@ -601,6 +601,7 @@ static int sdcardfs_setattr(struct vfsmount *mnt, struct dentry *dentry, struct 
 	struct dentry *parent;
 	struct inode tmp;
 	struct sdcardfs_inode_data *top;
+	struct dentry tmp_d;
 	const struct cred *saved_cred = NULL;
 
 	inode = d_inode(dentry);
@@ -629,9 +630,10 @@ static int sdcardfs_setattr(struct vfsmount *mnt, struct dentry *dentry, struct 
 	tmp.i_size = i_size_read(inode);
 	data_put(top);
 	tmp.i_sb = inode->i_sb;
+	tmp_d.d_inode = &tmp;
 
 	/*
-	 * Check if user has permission to change inode.  We don't check if
+	 * Check if user has permission to change dentry.  We don't check if
 	 * this user can change the lower inode: that should happen when
 	 * calling notify_change on the lower inode.
 	 */
@@ -641,7 +643,7 @@ static int sdcardfs_setattr(struct vfsmount *mnt, struct dentry *dentry, struct 
 	 * we have write access. Changes to mode, owner, and group are ignored
 	 */
 	ia->ia_valid |= ATTR_FORCE;
-	err = inode_change_ok(&tmp, ia);
+	err = setattr_prepare(&tmp_d, ia);
 
 	if (!err) {
 		/* check the Android group ID */
