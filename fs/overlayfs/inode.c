d0642b1e69f32b0458667f8a8b9a98cf90043a09
Miklos Szeredi <mszeredi@redhat.com>
2017-05-05 11:38:58 +0200

    ovl: merge getattr for dir and nondir
    [huexxx@gmail.com: old school vfs_getattr and ignore statx stuff]
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- fs/overlayfs/inode.c
+++ fs/overlayfs/inode.c
@@ -53,12 +53,13 @@ out:
 	return err;
 }
 
-static int ovl_getattr(struct vfsmount *mnt, struct dentry *dentry,
-			 struct kstat *stat)
+int ovl_getattr(struct vfsmount *mnt, struct dentry *dentry,
+		struct kstat *stat)
 {
 	enum ovl_path_type type;
 	struct path realpath;
 	const struct cred *old_cred;
+	bool is_dir = S_ISDIR(dentry->d_inode->i_mode);
 	int err;
 
 	type = ovl_path_real(dentry, &realpath);
@@ -80,11 +81,13 @@ static int ovl_getattr(struct vfsmount *mnt, struct dentry *dentry,
 	if (ovl_same_sb(dentry->d_sb)) {
 		if (OVL_TYPE_ORIGIN(type)) {
 			struct kstat lowerstat;
-
+#if 0
+			u32 lowermask = STATX_INO | (!is_dir ? STATX_NLINK : 0);
+#endif
 			ovl_path_lower(dentry, &realpath);
 #if 0
 			err = vfs_getattr(&realpath, &lowerstat,
-					  STATX_INO | STATX_NLINK, flags);
+					  lowermask, flags);
 #else
 			err = vfs_getattr(&realpath, &lowerstat);
 #endif
@@ -97,11 +100,32 @@ static int ovl_getattr(struct vfsmount *mnt, struct dentry *dentry,
 			 * upper files, so we cannot use the lower origin st_ino
 			 * for those different files, even for the same fs case.
 			 */
-			if (lowerstat.nlink == 1)
+			if (is_dir || lowerstat.nlink == 1)
 				stat->ino = lowerstat.ino;
 		}
 		stat->dev = dentry->d_sb->s_dev;
+	} else if (is_dir) {
+		/*
+		 * If not all layers are on the same fs the pair {real st_ino;
+		 * overlay st_dev} is not unique, so use the non persistent
+		 * overlay st_ino.
+		 *
+		 * Always use the overlay st_dev for directories, so 'find
+		 * -xdev' will scan the entire overlay mount and won't cross the
+		 * overlay mount boundaries.
+		 */
+		stat->dev = dentry->d_sb->s_dev;
+		stat->ino = dentry->d_inode->i_ino;
 	}
+
+	/*
+	 * It's probably not worth it to count subdirs to get the
+	 * correct link count.  nlink=1 seems to pacify 'find' and
+	 * other utilities.
+	 */
+	if (is_dir && OVL_TYPE_MERGE(type))
+		stat->nlink = 1;
+
 out:
 	revert_creds(old_cred);
 
