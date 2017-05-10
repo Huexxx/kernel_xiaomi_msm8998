a2879e76ea1ad5861a1b9899dd71b37370d51625
Christoph Hellwig <hch@lst.de>
2017-05-10 15:06:33 +0200

    fs: switch ->s_uuid to uuid_t
    For some file systems we still memcpy into it, but in various places this
    already allows us to use the proper uuid helpers.  More to come..
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Amir Goldstein <amir73il@gmail.com>
    Acked-by: Mimi Zohar <zohar@linux.vnet.ibm.com> (Changes to IMA/EVM)
    Reviewed-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -35,6 +35,7 @@
 #include <linux/percpu-rwsem.h>
 #include <linux/delayed_call.h>
 #include <linux/ioprio.h>
+#include <linux/uuid.h>
 
 #include <asm/byteorder.h>
 #include <uapi/linux/fs.h>
@@ -1450,8 +1451,8 @@ struct super_block {
 
 	struct sb_writers	s_writers;
 
-	char s_id[32];				/* Informational name */
-	u8 s_uuid[16];				/* UUID */
+	char			s_id[32];	/* Informational name */
+	uuid_t			s_uuid;		/* UUID */
 
 	void 			*s_fs_info;	/* Filesystem private info */
 	unsigned int		s_max_links;
