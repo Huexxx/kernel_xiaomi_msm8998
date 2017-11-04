7c9530735b7b084fcefccfa58b645388af42b822
Matthew Wilcox <willy@infradead.org>
2017-11-03 23:09:45 -0400

    xarray: Change definition of sibling entries
    Instead of storing a pointer to the slot containing the canonical entry,
    store the offset of the slot.  Produces slightly more efficient code
    (~300 bytes) and simplifies the implementation.
    
    [huexxx@gmail.com: ignore testing stuff]
    Signed-off-by: Matthew Wilcox <willy@infradead.org>
    Reviewed-by: Josef Bacik <jbacik@fb.com>

--- include/linux/radix-tree.h
+++ include/linux/radix-tree.h
@@ -60,12 +60,7 @@ static inline bool radix_tree_is_internal_node(void *ptr)
 
 #define RADIX_TREE_MAX_TAGS 3
 
-#ifdef __KERNEL__
-#define RADIX_TREE_MAP_SHIFT	(CONFIG_BASE_SMALL ? 4 : 6)
-#else
-#define RADIX_TREE_MAP_SHIFT	3	/* For more stressful testing */
-#endif
-
+#define RADIX_TREE_MAP_SHIFT	XA_CHUNK_SHIFT
 #define RADIX_TREE_MAP_SIZE	(1UL << RADIX_TREE_MAP_SHIFT)
 #define RADIX_TREE_MAP_MASK	(RADIX_TREE_MAP_SIZE-1)
 
