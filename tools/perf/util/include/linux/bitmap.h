c35fb125ea55389ca576d084e290d32d825d02e3
Matthew Wilcox <willy@infradead.org>
2017-11-10 15:15:08 -0500

    xarray: Add XArray unconditional store operations
    xa_store() differs from radix_tree_insert() in that it will overwrite an
    existing element in the array rather than returning an error.  This is
    the behaviour which most users want, and those that want more complex
    behaviour generally want to use the xas family of routines anyway.
    
    For memory allocation, xa_store() will first attempt to request memory
    from the slab allocator; if memory is not immediately available, it will
    drop the xa_lock and allocate memory, keeping a pointer in the xa_state.
    It does not use the per-CPU cache, although those will continue to exist
    until all radix tree users are converted to the xarray.
    
    This patch also includes xa_erase() and __xa_erase() for a streamlined
    way to store NULL.  Since there is no need to allocate memory in order
    to store a NULL in the XArray, we do not need to trouble the user with
    deciding what memory allocation flags to use.
    
    [huexxx@gmail.com: drop tools/include/linux/spinlock.h mods and testing stuff]
    Signed-off-by: Matthew Wilcox <willy@infradead.org>

--- tools/perf/util/include/linux/bitmap.h
+++ tools/perf/util/include/linux/bitmap.h
@@ -10,6 +10,7 @@
 int __bitmap_weight(const unsigned long *bitmap, int bits);
 void __bitmap_or(unsigned long *dst, const unsigned long *bitmap1,
 		 const unsigned long *bitmap2, int bits);
+void bitmap_clear(unsigned long *map, unsigned int start, int len);
 
 #define BITMAP_LAST_WORD_MASK(nbits)					\
 (									\
