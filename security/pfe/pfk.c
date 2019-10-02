0dd954ef0137cb4bfaca2912c0361f5abab6e88a
Jaegeuk Kim <jaegeuk@google.com>
2019-10-01 22:28:02 -0700

    ICE, pfk: fix to set dio_inode precisely
    When using loopback device with dio, we can't rely on page flag.
    
    Bug: 141601405
    Bug: 141860559
    Bug: 140882488
    Change-Id: I09526c25e8d5333853e777f29333f9fa8da37459
    Signed-off-by: Jaegeuk Kim <jaegeuk@google.com>

--- security/pfe/pfk.c
+++ security/pfe/pfk.c
@@ -213,7 +213,7 @@ static struct inode *pfk_bio_get_inode(const struct bio *bio)
 	/* Using direct-io (O_DIRECT) without page cache */
 	inode = dio_bio_get_inode((struct bio *)bio);
 	if (inode) {
-		pr_debug("inode on direct-io, inode = 0x%pK.\n", inode);
+		pr_debug("inode on direct-io, inode = 0x%p.\n", inode);
 
 		return inode;
 	}
