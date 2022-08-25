fc36a4d0efd1dae21c1164f463e15c4ad2a09dc8
Chengming Zhou <zhouchengming@bytedance.com>
2022-08-26 00:41:05 +0800

    sched/psi: Move private helpers to sched/stats.h
    This patch move psi_task_change/psi_task_switch declarations out of
    PSI public header, since they are only needed for implementing the
    PSI stats tracking in sched/stats.h
    
    psi_task_switch is obvious, psi_task_change can't be public helper
    since it doesn't check psi_disabled static key. And there is no
    any user now, so put it in sched/stats.h too.
    
    Signed-off-by: Chengming Zhou <zhouchengming@bytedance.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Link: https://lore.kernel.org/r/20220825164111.29534-5-zhouchengming@bytedance.com

--- kernel/sched/stats.h
+++ kernel/sched/stats.h
@@ -48,6 +48,10 @@ rq_sched_info_depart(struct rq *rq, unsigned long long delta)
 #endif
 
 #ifdef CONFIG_PSI
+void psi_task_change(struct task_struct *task, int clear, int set);
+void psi_task_switch(struct task_struct *prev, struct task_struct *next,
+		     bool sleep);
+
 /*
  * PSI tracks state that persists across sleeps, such as iowaits and
  * memory stalls. As a result, it has to distinguish between sleeps,
