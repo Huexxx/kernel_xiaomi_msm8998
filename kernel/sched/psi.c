08fa7420a55edf8fa769a5e51d69b35f45e35a87
Christoph Hellwig <hch@lst.de>
2022-09-15 10:41:57 +0100

    sched/psi: export psi_memstall_{enter,leave}
    To properly account for all refaults from file system logic, file systems
    need to call psi_memstall_enter directly, so export it.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Link: https://lore.kernel.org/r/20220915094200.139713-3-hch@lst.de
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- kernel/sched/psi.c
+++ kernel/sched/psi.c
@@ -1060,6 +1060,7 @@ void psi_memstall_enter(unsigned long *flags)
 
 	rq_unlock_irq(rq, &rf);
 }
+EXPORT_SYMBOL_GPL(psi_memstall_enter);
 
 /**
  * psi_memstall_leave - mark the end of an memory stall section
@@ -1089,6 +1090,7 @@ void psi_memstall_leave(unsigned long *flags)
 
 	rq_unlock_irq(rq, &rf);
 }
+EXPORT_SYMBOL_GPL(psi_memstall_leave);
 
 #ifdef CONFIG_CGROUPS
 int psi_cgroup_alloc(struct cgroup *cgroup)
