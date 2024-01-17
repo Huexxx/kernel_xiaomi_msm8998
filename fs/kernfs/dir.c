70e1a1cad3db628372e64ce1496a9a1f1c5780b6
Huexxx <huexxx@gmail.com>
2024-01-17 19:40:03 +0100

    kernfs: fs: support RENAME_NOREPLACE for local filesystems

--- fs/kernfs/dir.c
+++ fs/kernfs/dir.c
@@ -1124,12 +1124,16 @@ static int kernfs_iop_rmdir(struct inode *dir, struct dentry *dentry)
 }
 
 static int kernfs_iop_rename(struct inode *old_dir, struct dentry *old_dentry,
-			     struct inode *new_dir, struct dentry *new_dentry)
+			     struct inode *new_dir, struct dentry *new_dentry,
+			     unsigned int flags)
 {
 	struct kernfs_node *kn = kernfs_dentry_node(old_dentry);
 	struct kernfs_node *new_parent = new_dir->i_private;
 	struct kernfs_syscall_ops *scops = kernfs_root(kn)->syscall_ops;
 	int ret;
+
+	if (flags & ~RENAME_NOREPLACE)
+		return -EINVAL;
 
 	if (!scops || !scops->rename)
 		return -EPERM;
