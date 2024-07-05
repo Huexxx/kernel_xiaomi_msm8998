1a517676fb06207a04f95d6888c4e3dd6fb4cb0f
Huexxx <huexxx@gmail.com>
2024-07-05 22:17:05 +0200

    Revert "mm: coalesce split strings"
    This reverts commit 34105218d244f200ac3d5de7822cddd7bac791f5.

--- mm/mmap.c
+++ mm/mmap.c
@@ -2759,7 +2759,7 @@ SYSCALL_DEFINE5(remap_file_pages, unsigned long, start, unsigned long, size,
 	struct file *file;
 
 	pr_warn_once("%s (%d) uses deprecated remap_file_pages() syscall. See Documentation/vm/remap_file_pages.txt.\n",
-		     current->comm, current->pid);
+			current->comm, current->pid);
 
 	if (prot)
 		return ret;
