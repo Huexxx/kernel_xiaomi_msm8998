43a01f04914bbb487698be4d40cb97aa3f4683e1
Huexxx <huexxx@gmail.com>
2024-07-05 22:20:11 +0200

    *** Revert "mm/readahead.c, mm/vmscan.c: use lru_to_page instead of list_to_page"
    This reverts commit 7a880b290b151a1fde30b070df6dedba8a925a0a.
    
    Conflicts:
            mm/readahead.c

--- mm/readahead.c
+++ mm/readahead.c
@@ -33,6 +33,8 @@ file_ra_state_init(struct file_ra_state *ra, struct address_space *mapping)
 }
 EXPORT_SYMBOL_GPL(file_ra_state_init);
 
+#define list_to_page(head) (list_entry((head)->prev, struct page, lru))
+
 /*
  * see if a page needs releasing upon read_cache_pages() failure
  * - the caller of read_cache_pages() may have set PG_private or PG_fscache
@@ -63,7 +65,7 @@ static void read_cache_pages_invalidate_pages(struct address_space *mapping,
 	struct page *victim;
 
 	while (!list_empty(pages)) {
-		victim = lru_to_page(pages);
+		victim = list_to_page(pages);
 		list_del(&victim->lru);
 		read_cache_pages_invalidate_page(mapping, victim);
 	}
@@ -86,7 +88,7 @@ int read_cache_pages(struct address_space *mapping, struct list_head *pages,
 	int ret = 0;
 
 	while (!list_empty(pages)) {
-		page = lru_to_page(pages);
+		page = list_to_page(pages);
 		list_del(&page->lru);
 		if (add_to_page_cache_lru(page, mapping, page->index,
 				readahead_gfp_mask(mapping))) {
