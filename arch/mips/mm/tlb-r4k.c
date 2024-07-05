1de5be9f567dc4b80bc5ada272128055ec7c61c8
Huexxx <huexxx@gmail.com>
2024-07-06 01:40:52 +0200

    Revert "arch: fix has_transparent_hugepage()"
    This reverts commit c7d0d2f6f46a5b5ce3f8f151eb6369ad8139c93a.

--- arch/mips/mm/tlb-r4k.c
+++ arch/mips/mm/tlb-r4k.c
@@ -399,20 +399,19 @@ void add_wired_entry(unsigned long entrylo0, unsigned long entrylo1,
 
 #ifdef CONFIG_TRANSPARENT_HUGEPAGE
 
-int has_transparent_hugepage(void)
+int __init has_transparent_hugepage(void)
 {
-	static unsigned int mask = -1;
+	unsigned int mask;
+	unsigned long flags;
 
-	if (mask == -1) {	/* first call comes during __init */
-		unsigned long flags;
+	local_irq_save(flags);
+	write_c0_pagemask(PM_HUGE_MASK);
+	back_to_back_c0_hazard();
+	mask = read_c0_pagemask();
+	write_c0_pagemask(PM_DEFAULT_MASK);
 
-		local_irq_save(flags);
-		write_c0_pagemask(PM_HUGE_MASK);
-		back_to_back_c0_hazard();
-		mask = read_c0_pagemask();
-		write_c0_pagemask(PM_DEFAULT_MASK);
-		local_irq_restore(flags);
-	}
+	local_irq_restore(flags);
+
 	return mask == PM_HUGE_MASK;
 }
 
